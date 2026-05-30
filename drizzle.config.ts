1031f7ba85aee88304041f66ba071dfda17cdccb
Sunny Golovine <sunny@golovinemail.org>
2026-05-30 09:57:46 -0600

    chore(db): move schema files to top-level schema folder

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -3,7 +3,7 @@ import { defineConfig } from "drizzle-kit";
 export const databaseUrl = process.env.DB_FILE_NAME ?? "file:./data/cc-offers.sqlite";
 
 export default defineConfig({
-  schema: "./src/db/schema.ts",
+  schema: "./schema/schema.ts",
   out: "./drizzle",
   dialect: "sqlite",
   dbCredentials: {
